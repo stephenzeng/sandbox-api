71959e6b37fbe529db667879285fbd1d9eb2089a
unknown <zeng.stephen@gmail.com>
2018-02-02 10:29:39 +1100

    Init project

---
+++ Controllers/FooController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sandbox_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FooController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "0.0.1";
+        }
+    }
+}
